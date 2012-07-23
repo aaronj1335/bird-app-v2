4aea7d3f01eb0f0af33d606ac20d81b69f7ff309
Aaron Stacy <aaron.r.stacy@gmail.com>
2012-07-23 11:55:34 -0500

    made bluejay more succinct

--- static/bluejay.js
+++ static/bluejay.js
@@ -2,9 +2,7 @@ define([
     'bird/bird'
 ], function(birdifyIt) {
     return function(node) {
-        var childNodes;
-        birdifyIt(node);
-        childNodes = node.childNodes;
+        var childNodes = birdifyIt(node).childNodes;
         childNodes[childNodes.length-1].style.backgroundColor = 'blue';
     };
 });
